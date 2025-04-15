4768bd71b320576ededd8e5314cc01707173917d
Yiping Deng <ydeng@hubspot.com>
2025-04-15 17:26:45 +0100

    Add 1d block tiling example

--- faster-matmul/1d_block_tiling_matmul.cu
+++ faster-matmul/1d_block_tiling_matmul.cu
@@ -1,50 +1,78 @@
 #include "faster_matmul.cuh"
 
-#define BLOCK_SIZE 32
+#define CEIL_DIV(M, N) (((M) + (N) - 1) / (N))
 
-__global__ void sram_matmul(float* A, float* B, float* C, int M, int N, int K) {
-    __shared__ float shared_A[BLOCK_SIZE][BLOCK_SIZE];
-    __shared__ float shared_B[BLOCK_SIZE][BLOCK_SIZE];
+template <int BM, int BN, int BK, int TM>
+__global__ void block_tiling_matmul_1d(float* A, float* B, float* C, const int M, const int N, const int K) {
+    __shared__ float shared_A[BM][BK];
+    __shared__ float shared_B[BK][BN];
 
-    // threadIdx.x changes in the same warp
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
+    // blockIdx.x is the block id in the N dimension, aka the column index of the block
+    // blockIdx.y is the block id in the M dimension, aka the row index of the block
 
+    // Each warp will calculate 32 * TM elements, with 32 being the columnar dim.
+    // Num threads = BM * BN / TM, we will 1d tiling on the M dimension.
+    const int thread_col = threadIdx.x % BN;
+    const int thread_row = threadIdx.x / BN;
 
-    if (i < M && j < N) {
-        // sliding window of BLOCK_SIZE x BLOCK_SIZE
-        // sliding on the K dimension
-        float sum = 0.0f;
-        for (int w_idx = 0; w_idx < (K + BLOCK_SIZE - 1) / BLOCK_SIZE; w_idx++) {
-            int k_start = w_idx * BLOCK_SIZE;
+    // Move blocktile to beginning of A's row and B's column
+    A += blockIdx.y * BM * K;
+    B += blockIdx.x * BN;
+    C += blockIdx.y * BM * N + blockIdx.x * BN;
 
-            // init shared_A, shared_B and shared_C
-            // Check bounds for shared_A and shared_B, add assign value to 0 if out of bounds
-            if (k_start + threadIdx.x < K) {
-                shared_A[threadIdx.y][threadIdx.x] = A[i * K + k_start + threadIdx.x];
+    const uint inner_col_a = threadIdx.x % BK; // warp-level GMEM coalescing
+    const uint inner_row_a = threadIdx.x / BK;
+    const uint inner_col_b = threadIdx.x % BN; // warp-level GMEM coalescing
+    const uint inner_row_b = threadIdx.x / BN;
+
+    float thread_results[TM] = {0.0f};
+
+    // Assume K is divisible by BK
+    for (uint bkIdx = 0; bkIdx < K; bkIdx += BK) {
+        // Load A and B tiles into shared memory j
+        shared_A[inner_row_a][inner_col_a] = A[inner_row_a * K + inner_col_a];
+        shared_B[inner_row_b][inner_col_b] = B[inner_row_b * N + inner_col_b];
+
+        __syncthreads();
+
+        // advance blocktile
+        A += BK;
+        B += BK * N;
+
+        // Perform matrix multiplication
+        for (uint dot_idx = 0; dot_idx < BK; dot_idx++) {
+            // This is cached & reused for each thread in the warp
+            float tmp = shared_B[dot_idx][thread_col];
+
+            // We are reading TM elemets from A[thread_row * TM : thread_row * TM + TM][dot_idx]
+            // and multiply with the cached B[thread_col][dot_idx]
+            #pragma unroll
+            for (uint res_idx = 0; res_idx < TM; res_idx++) {
+                thread_results[res_idx] += shared_A[thread_row * TM + res_idx][dot_idx] * tmp;
             }
-
-            if (k_start + threadIdx.y < K) {
-                shared_B[threadIdx.y][threadIdx.x] = B[(k_start + threadIdx.y) * N + j];
-            }
-
-            __syncthreads(); // wait for all threads to finish loading data
-            for (int k = 0; k < BLOCK_SIZE && (k_start + k) < K; k++) {
-                sum += shared_A[threadIdx.y][k] * shared_B[k][threadIdx.x];
-            }
-            __syncthreads(); // avoid over-writing shared_A and shared_B before all threads finish the computation
         }
-        C[i * N + j] = sum;
+        __syncthreads();
+    }
+
+    #pragma unroll
+    for (uint res_idx = 0; res_idx < TM; res_idx++) {
+        C[(thread_row * TM + res_idx) * N + thread_col] = thread_results[res_idx];
     }
 }
 
 // Kernel launcher function
-void launch_sram_matmul(float* d_A, float* d_B, float* d_C, int m, int n, int k, cudaStream_t stream) {
-    dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE);
-    // gridDim is reversed because we want to access the memory with changing j first instead of 
-    dim3 gridDim((n + blockDim.x - 1) / blockDim.x, (m + blockDim.y - 1) / blockDim.y);
+void launch_1d_block_tiling_matmul(float* d_A, float* d_B, float* d_C, int m, int n, int k, cudaStream_t stream) {
+    constexpr int BM = 64;
+    constexpr int BN = 64;
+    constexpr int BK = 8;
+    constexpr int TM = 8;
+
+    dim3 blockDim(BM * BN / TM);
+    // Reversing order to optimize L2 cache access. Grid will move on the N dimension fast and M dimension slow.
+    // With row-major layout, this is more cache-friendly.
+    dim3 gridDim(CEIL_DIV(n, BN), CEIL_DIV(m, BM));
     
-    sram_matmul<<<gridDim, blockDim, 0, stream>>>(d_A, d_B, d_C, m, n, k);
+    block_tiling_matmul_1d<BM, BN, BK, TM><<<gridDim, blockDim, 0, stream>>>(d_A, d_B, d_C, m, n, k);
 }
 
 int main() {
@@ -53,13 +81,12 @@ int main() {
     int n = 2048; // Matrix B: k x n, Matrix C: m x n
     int k = 512;
     
-    std::cout << "Running sram bank conflict free matrix multiplication benchmark:" << std::endl;
+    std::cout << "Running 1D block tiling matrix multiplication benchmark:" << std::endl;
     
     // Run the benchmark with the naive matrix multiplication kernel
     float avg_time = run_benchmark<float>(
-        launch_sram_matmul, m, n, k
+        launch_1d_block_tiling_matmul, m, n, k
     );
     
     return 0;
 }
-
